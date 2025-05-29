c7eb16ac25452f3a7bdc8a03692d2fbbb403e434
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-05-30 02:10:00 +0300

    Add rudimentary stub of the main module of the daemon.

---
+++ src/api-lite-core.cr
@@ -0,0 +1,23 @@
+#
+# src/api-lite-core.cr
+# =============================================================================
+# Customers API Lite microservice prototype (Crystal port). Version 0.0.1
+# =============================================================================
+# A daemon written in Crystal, designed and intended to be run
+# as a microservice, implementing a special Customers API prototype
+# with a smart yet simplified data scheme.
+# =============================================================================
+# (See the LICENSE file at the top of the source tree.)
+#
+
+# The main module of the daemon -----------------------------------------------
+
+module Core
+    def init()
+        puts()
+    end
+end
+
+include Core; init()
+
+# vim:set nu et ts=4 sw=4:
