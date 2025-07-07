593e57335c6e033bcb7beca7246cf28af9ecf640
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-07-07 19:27:10 +0300

    Add rudimentary helper module for the daemon: Define daemon settings pieces to grab. Define helper function to grab them.

---
+++ src/api-lite-helper.cr
@@ -0,0 +1,45 @@
+#
+# src/api-lite-helper.cr
+# =============================================================================
+# Customers API Lite microservice prototype (Crystal port). Version 0.0.1
+# =============================================================================
+# A daemon written in Crystal, designed and intended to be run
+# as a microservice, implementing a special Customers API prototype
+# with a smart yet simplified data scheme.
+# =============================================================================
+# (See the LICENSE file at the top of the source tree.)
+#
+
+# The helper module for the daemon --------------------------------------------
+
+require "toml"
+
+module Helper
+    # The path and filename of the daemon settings.
+    SETTINGS = "./etc/settings.conf"
+
+    # Daemon settings keys for the microservice daemon name.
+    DAEMON_NAME_G = "daemon"
+    DAEMON_NAME_S = "name"
+
+    # Daemon settings keys for the server port number.
+    SERVER_PORT_G = "server"
+    SERVER_PORT_S = "port"
+
+    # Daemon settings keys for the debug logging enabler.
+    LOG_ENABLED_G  = "logger"
+    LOG_ENABLED_S1 = "debug"
+    LOG_ENABLED_S2 = "enabled"
+
+    # Daemon settings keys for the SQLite database path.
+    DB_PATH_G  = "sqlite"
+    DB_PATH_S1 = "database"
+    DB_PATH_S2 = "path"
+
+    # Helper function. Used to get the daemon settings.
+    def _get_settings()
+        settings = TOML.parse(File.read(SETTINGS))
+    end
+end
+
+# vim:set nu et ts=4 sw=4:
