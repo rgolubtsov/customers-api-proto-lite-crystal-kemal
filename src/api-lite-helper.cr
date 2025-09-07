45982d9b6bad3ee8aed9d8e39b973c92c20d4562
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-09-08 00:10:00 +0300

    Bump version number to 0.1.12.

--- src/api-lite-helper.cr
+++ src/api-lite-helper.cr
@@ -1,7 +1,7 @@
 #
 # src/api-lite-helper.cr
 # =============================================================================
-# Customers API Lite microservice prototype (Crystal port). Version 0.1.10
+# Customers API Lite microservice prototype (Crystal port). Version 0.1.12
 # =============================================================================
 # A daemon written in Crystal, designed and intended to be run
 # as a microservice, implementing a special Customers API prototype
