20ab6681bb108179d12af56bfb4f885faffc12e2
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2026-02-10 15:25:00 +0300

    Get rid of unneeded SQL query..........................

--- src/api-lite-model.cr
+++ src/api-lite-model.cr
@@ -88,8 +88,7 @@ module Model
         "       customers      cust"               +
         " where"                                   +
         "      (cust.id = emails.customer_id) and" +
-        "      (cust.id =                  ?)",
-        "select name from customers where (id = ?)"]
+        "      (cust.id =                  ?)"]
 
     # The intermediate part of an SQL query,
     # used to order contact records by ID.
